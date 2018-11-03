bcf9f1d4cfd45b999a7d3f7d182149ef35aa9cdd
AncajimaTi <39843457+AncajimaTi@users.noreply.github.com>
2018-11-02 19:01:41 -0500

    Carpeta:Tienda,Proveedor,Trabajador,MateriaPrima

--- app/Trabajador.php
+++ app/Trabajador.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trabajador extends Model
 {
-    //
+    protected $table='Trabajador';
+    protected $primaryKey='idTrabajador';
+    public $timestamps=false;
+
+    protected $filleable=[
+		'idTipo_Persona',
+		'idPersona',
+		'sueldo',
+		'fecha_inicio',
+		'fecha_fin',	
+		'estado_idEstado'	
+
+    ];
+    protected $guarded =[
+     
+   ];
 }
